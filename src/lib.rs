29a18e999bef03bbf5551a7ff6dab61e05482639
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2026-02-12 17:19:43 -0700

    made simple game plugin

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+use bevy::prelude::*;
+
+pub struct FallingSand;
+impl Plugin for FallingSand {
+    fn build(&self, app: &mut App) {
+        app //
+            .add_plugins(DefaultPlugins)
+            .add_systems(Startup, setup)
+            .add_systems(
+                Update,
+                (
+                    hello_world, //
+                ),
+            );
+    }
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d::default());
+    info!("setup complete");
+}
+
+fn hello_world() {
+    info!("hello world!")
+}
