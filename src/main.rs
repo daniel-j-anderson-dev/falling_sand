91143352db7018d2338f07b1b0807c69ced4c3fe
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2026-02-14 19:23:49 -0700

    test game of life

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use bevy::prelude::*;
 
-use falling_sand::FallingSand;
+use falling_sand::{FallingSand, game_of_life::GameOfLife};
 
 fn main() {
-    match App::new().add_plugins(FallingSand).run() {
+    match App::new()
+        .add_plugins(GameOfLife) //
+        // .add_plugins(FallingSand) //
+        .run()
+    {
         AppExit::Success => info!("good bye"),
         AppExit::Error(n) => error!("exited with error code: {n}"),
     }
